7bb67cc9854ac557644142fc178d2e218877d7de
Tomas Dolezal <tomas.dolezal@gmail.com>
2016-07-14 23:24:31 +0200

    Update nuspec keywords

--- src/SolutionInfo.fs
+++ src/SolutionInfo.fs
@@ -2,9 +2,10 @@
 open System.Reflection
 
 [<assembly: AssemblyVersionAttribute("1.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] Version = "1.0.0"
+    let [<Literal>] InformationalVersion = "1.1.0"
